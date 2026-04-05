fd0ce04a6c57b723bc2c05ab8aacd43bbb9cdf7d
Rosan <112734885+2002rosan@users.noreply.github.com>
2026-04-05 15:14:26 +0000

    1st commit

---
+++ react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section className='bg-mauve-950 text-zinc-100 h-screen w-screen align-middle p-10 flex justify-center'>
+        <h1 className='w-fit h-fit text-5xl'>Here goes the practice ground!</h1>
+      </section>
+    </>
+  )
+}
+
+export default App
